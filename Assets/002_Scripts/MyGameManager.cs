a806db7651a75bc6e341d89b0ffddab5f50bde06
MichiakiNagai <nagai.michiaki.xr@gmail.com>
2023-07-06 01:46:30 +0900

    V0.21：UI テキスト5つ追加。それに伴い、MyGameManager修正。進捗動画2を収録。

--- Assets/002_Scripts/MyGameManager.cs
+++ Assets/002_Scripts/MyGameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MyGameManager : MonoBehaviour
@@ -23,12 +24,21 @@ public class MyGameManager : MonoBehaviour
     public GameObject sus_Kyoto;
     public GameObject theater;
 
+    public GameObject[] textList;
+
     public float time = -10.0f;
+
+    int text_i = 0;
 
     void Start()
     {
         part_CountDown.SetActive(true);
         train.SetActive(false);
+
+        for (int i = 0; i < textList.Length; i++)
+        {
+            textList[i].SetActive(false);
+        }
     }
 
     
@@ -36,6 +46,40 @@ public class MyGameManager : MonoBehaviour
     {
         time = Time.time;
 
+        SwitchModels();
+
+        if (26.50f <= time && time < 34.00f)
+        {
+            DisplayTexts(0);
+        }
+        else if (34.00f <= time && time < 40.00f)
+        {
+            DisplayTexts(1);
+        }
+        else if (40.00f <= time && time < 47.00f)
+        {
+            DisplayTexts(2);
+        }
+        else if (47.00f <= time && time < 52.00f)
+        {
+            DisplayTexts(3);
+        }/*
+        else if (50.00f <= time && time < 50.00f)
+        {
+            DisplayTexts(4);
+        }*/
+        else if (55.00f <= time && time < 60.00f)
+        {
+            DisplayTexts(4);
+        }
+        else if (63.50f <= time && time < 70.00f)
+        {
+            DisplayTexts(5);
+        }
+    }
+
+    void SwitchModels()
+    {
         if (26.5f < time)
         {
             part_CountDown.SetActive(false);
@@ -50,10 +94,10 @@ public class MyGameManager : MonoBehaviour
             else if (50.0f <= time && time < 70.0f)
             {
                 sus_Kyoto.SetActive(true);
-                theater.SetActive(true);
                 if (60.0f < time)
                 {
                     tunnel.SetActive(false);
+                    theater.SetActive(true);
                 }
             }
             else if (70.0f <= time)
@@ -61,8 +105,15 @@ public class MyGameManager : MonoBehaviour
                 sus_Kyoto.SetActive(false);
             }
         }
-        
+    }
 
-        
+    void DisplayTexts(int idx)
+    {
+        if (idx != 0)
+        {
+            textList[idx -1].SetActive(false);
+        }
+        textList[idx].SetActive(true);
+        idx++;
     }
 }
