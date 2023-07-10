c258556809d510a524c1ac35f66b61f7d24ce44e
MichiakiNagai <nagai.michiaki.xr@gmail.com>
2023-07-10 10:56:46 +0900

    V0.40：「SUS_AWARD」作成。本編入ったときにフェードインしたいかも。橋の上にいる時、専用SEを流す仕様を追加。Train のBody に名称振り。Main BGM の音量0.45→0.4へ変更。「Train」タグ追加（橋の上にいる判定の為）。不要なBGM削除。

--- Assets/002_Scripts/MyGameManager.cs
+++ Assets/002_Scripts/MyGameManager.cs
@@ -31,11 +31,13 @@ public class MyGameManager : MonoBehaviour
     public GameObject[] textList;
 
     public Animator cameraAnimator;
+    public AudioSource MainTrainSE;
+    public AudioSource bridgeSE;
 
     public float time = -10.0f;
 
-    float exitCountDownTime = 30.20f;  // è]óàtime(25.20f) ÇÊÇË +5.0f ÇµÇΩÇ™ÅAÇ‡Ç§ +5.0f Ç…ÇµÇΩï˚Ç™ÇÊÇ¢Ç©Ç‡
-    float switchModelTime = 44.5f;
+    float exitCountDownTime = 30.20f;  // è]óàtime(25.20f) ÇÊÇË +5.0f ÇµÇΩ
+    float switchModelTime = 49.50f;
     int text_i = 0;
 
     bool istunnel_B_ON_Flag = false;
@@ -64,7 +66,7 @@ public class MyGameManager : MonoBehaviour
             part_CountDown.SetActive(false);
             part_Title.SetActive(true);
         }
-        else if (exitCountDownTime < time && time < 83.0f)
+        else if (exitCountDownTime + 5.0f < time && time < 89.0f)
         {
             part_Title.SetActive(false);
             SwitchModels();
@@ -87,17 +89,17 @@ public class MyGameManager : MonoBehaviour
 
     void SwitchModels()
     {
-        if (exitCountDownTime < time)  // 30.20f
+        if (exitCountDownTime < time)  // 35.20f
         {
             train.SetActive(true);
 //            presidentVoice.SetActive(true);
             part_CountDown.SetActive(false);
 
-            if (switchModelTime <= time)  // 44.50f
+            if (switchModelTime <= time)  // 49.50f
             {
                 seto_Bridge.SetActive(false);
 
-                if (switchModelTime +10.50f <= time) // 55.0f
+                if (switchModelTime +10.50f <= time) // 60.00f
                 {
                     if (!istunnel_B_ON_Flag)
                     {
@@ -105,17 +107,17 @@ public class MyGameManager : MonoBehaviour
                         istunnel_B_ON_Flag = true;
                     }
 
-                    if (switchModelTime + 12.00f <= time)  // 56.5f
+                    if (switchModelTime + 12.00f <= time)  // 61.50f
                     {
                         tunnel_A.SetActive(false);
 
-                        if (switchModelTime + 13.50f <= time)  // 58.0f
+                        if (switchModelTime + 13.50f <= time)  // 63.00f
                         {
                             sus_Kyoto.SetActive(true);
 
-                            if (time < switchModelTime + 30.50f)  // 75.0f
+                            if (time < switchModelTime + 30.50f)  // 80.00f
                             {
-                                if (switchModelTime + 18.30f < time)  // 62.8f
+                                if (switchModelTime + 18.30f < time)  // 67.80f
                                 {
                                     tunnel_B.SetActive(false);
                                     telsaHall.SetActive(true);
